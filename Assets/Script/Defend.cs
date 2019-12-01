0fbf9277f77cd4dbd86357eb63871869a19a30ed
haco77 <xjp8340@gmail.com>
2019-12-01 00:34:13 -0500

    New Model And shield logic

---
+++ Assets/Script/Defend.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class Defend : MonoBehaviour {
+    private GlobalLogic global;
+	void Start () {
+        GameObject tmp = GameObject.Find("Global");
+        if (tmp) {
+            global = tmp.GetComponent<GlobalLogic>();
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject victim = collision.collider.gameObject;
+        
+        if (victim.CompareTag("bomb"))
+        {
+            Destroy(victim);
+            return;
+        }
+    }
+}
