661534a541b18fb02807104071ae942865a6f9e8
xiangshidai <xiangshi@seas.upenn.edu>
2019-12-07 15:54:28 -0500

    cut sound & alert sound

--- Assets/SoundEffect/cutSound.cs
+++ Assets/SoundEffect/cutSound.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(AudioSource))]
 public class cutSound : MonoBehaviour
 {
     // Start is called before the first frame update
