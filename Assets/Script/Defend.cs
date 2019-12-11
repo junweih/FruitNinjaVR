62d576d285adff0e9d422f553ca660683f9306b4
Junwei Huang <junweihuang49@gmail.com>
2019-12-11 17:00:46 -0500

    .

--- Assets/Script/Defend.cs
+++ Assets/Script/Defend.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Valve.VR;
 
 [RequireComponent (typeof(Rigidbody))]
 public class Defend : MonoBehaviour {
     private GlobalLogic global;
-	void Start () {
+    public SteamVR_Action_Vibration hapticAction;
+    private bool hapticStart = false;
+    void Start () {
         GameObject tmp = GameObject.Find("Global");
         if (tmp) {
             global = tmp.GetComponent<GlobalLogic>();
         }
-	}
+    }
+
+    private void LeftHandPulse(float duratiton, float frequencey, float amplitute)
+    {
+        hapticAction.Execute(0, duratiton, frequencey, amplitute, SteamVR_Input_Sources.LeftHand);
+    }
+    private float timer = 0f;
+    private void Update()
+    {
+        if (hapticStart)
+        {
+            LeftHandPulse(1.5f, 1f, 1f);
+            timer += Time.deltaTime;
+            if (timer > 0.50f)
+            {
+                timer = 0f;
+                hapticStart = false;
+            }
+        }
+
+
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -17,6 +41,7 @@ public class Defend : MonoBehaviour {
         
         if (victim.CompareTag("bomb"))
         {
+            hapticStart = true;
             victim.GetComponent<BombScript>().died = true;
             return;
         }
